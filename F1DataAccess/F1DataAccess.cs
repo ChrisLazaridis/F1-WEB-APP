8df08584bc85ef3d2f62eef049537849b805c74a
Christos Lazaridis <clazaridis7@gmail.com>
2023-12-18 02:27:41 +0200

    Add .gitignore to exclude sensitive files and created the MySQL connection

--- F1DataAccess/F1DataAccess.cs
+++ F1DataAccess/F1DataAccess.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.Data.Sqlite;
-using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
 namespace F1DataAccess
 {
     public class F1DataAccess
     {
+        private readonly F1DataAccessConfiguration _configuration;
 
+        public F1DataAccess(F1DataAccessConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void YourDatabaseMethod()
+        {
+            string connectionString = _configuration.GetConnectionString();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                //TODO: Do stuff with the db 
+
+                connection.Close();
+            }
+        }
     }
 }
